cd8b93ac514aaf814adb4cb0dd373b1e9fcf95b3
fangchuangbin <fangchuangbin@qq.com>
2020-10-30 15:29:38 +0800

    master

--- app/middleware/token.js
+++ app/middleware/token.js
@@ -1,7 +1,6 @@
 module.exports = (options, app) => {
   return async function token(ctx, next) {
     var tokenData = ctx.cookies.get('loginToken');
-    console.log(tokenData);
     await next();
   }
 }
